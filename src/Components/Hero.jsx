4d05228cfede28fd9308cba240ac8a756535e2db
Neha Methani <nehamethani54@gmail.com>
2024-11-12 23:55:45 +0530

    Home page refined

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -4,10 +4,10 @@ import HomePageImg from '../assets/IMG_6725.PNG';
 const Hero = () => {
   return (
     <>
-      <section className='bg-black flex flex-col-reverse md:flex-row h-98 items-center justify-center' id="hero">
+      <section className='bg-black flex flex-col-reverse md:flex-row h-[calc(100vh-4rem)] items-center justify-center' id="hero">
 
-        <div className='flex flex-col flex-1 h-96 items-left ml-16 pl-14 pr-14 space-y-6 md:text-left md:ml-0 md:mr-0'>
-            <h1 className="text-slate-300 text-5xl font-bold uppercase tracking-wider md:text-left">Your Home's Exclusive Personal Trainer</h1>
+        <div className='flex flex-col flex-1 h-96 items-left justify-center text-center ml-10 p-24 space-y-6 md:text-left md:ml-0 md:mr-0'>
+            <h1 className="text-slate-300 text-4xl font-bold uppercase tracking-wider md:text-left">Your Home's Exclusive Personal Trainer</h1>
             <p className='text-slate-300 text-xl font-mono font-thin tracking-wider text-lg md:text-left'>High End Exercise Coaching</p>
             <br/>
             <button className='mt-auto self-start px-4  justify-self-end py-2 w-52 h-16 border border-slate-300 text-slate-50 hover:bg-slate-300 hover:text-black'>Book Your Session Now</button>   
