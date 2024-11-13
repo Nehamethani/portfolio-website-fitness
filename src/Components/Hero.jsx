b9f96e193f1c9973aca9a537f21995a2dbe8d003
Neha Methani <nehamethani54@gmail.com>
2024-11-13 23:52:54 +0530

    Responsive design

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -5,13 +5,13 @@ import { Link } from 'react-scroll';
 const Hero = () => {
   return (
     <>
-      <section className='bg-black flex flex-col-reverse md:flex-row h-[calc(100vh-4rem)] items-center justify-center' id="hero">
+      <section className='bg-black flex flex-col-reverse md:flex-row justify-center md:h-vh top-0' id="hero">
 
-        <div className='flex flex-col flex-1 h-96 items-left justify-center text-center ml-10 pl-24 space-y-6 md:text-left md:ml-0 md:mr-0'>
-            <h1 className="text-slate-300 text-5xl font-thin uppercase tracking-wider md:text-left" style={{fontFamily:"Funnel Display"}}>Your Home's Exclusive Personal Trainer</h1>
-            <p className='text-slate-300 text-xl font-mono font-thin text-lg md:text-left' style={{fontFamily:"Lexend Zetta"}}>High End Exercise Coaching</p>
+        <div className='flex flex-col flex-1 -mt-48 items-left justify-center lg:ml-10 lg:pl-24 space-y-6 lg:mt-0'>
+            <h1 className="text-white text-5xl font-thin uppercase tracking-wider text-center lg:text-left lg:text-5xl lg:p-0" style={{fontFamily:"Funnel Display"}}>Your Home's Exclusive Personal Trainer</h1>
+            <p className='text-slate-300 text-sm lg:text-xl font-mono font-thin text-lg text-center lg:text-left' style={{fontFamily:"Lexend Zetta"}}>High End Exercise Coaching</p>
             <br/>
-            <Link className='mt-auto text-center self-start px-4  justify-self-end py-2 w-52 h-16 border border-slate-300 text-slate-50 hover:bg-slate-300 hover:text-black'
+            <Link className='mt-auto text-center self-start px-4 justify-self-end py-2 w-52 h-16 border border-slate-300 text-slate-50 hover:bg-slate-300 hover:text-black'
             to="contact"   
             spy={true} 
             smooth={true} 
@@ -21,10 +21,10 @@ const Hero = () => {
             
             </Link>   
         </div> 
-
-        <div className='h-[calc(100vh-4rem)] md:w-1/2'>
+{/* w-full h-full opacity-75 rounded-lg shadow-lg object-cover */}
+        <div className='md:w-1/2 opacity-50'>
         <img
-                className="w-full h-full opacity-75 rounded-lg shadow-lg object-cover"
+                className="w-full md:w-1/2 lg:w-full mx-auto"
                 src={HomePageImg}
                 alt="React Jobs"
               />
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,33 +1,98 @@
-import React from 'react'
+import React, { useState } from 'react';
+import Logo from '../assets/logo.png';
 import { Link } from 'react-scroll';
 
-const style = {
-  backgroundColor:'#262626',
-
-}
-
 const Navbar = () => {
-    const NavLinkClass = ({ isActive }) => isActive ? 
-    'bg-black text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2' : 
-    'text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2' ;
+  const [isNavOpen, setIsNavOpen] = useState(false);
 
   return (
-    <nav className="bg-gray-500 z-10 relative sticky h-20 top-0 left-0" style={style}>
-        <div className="flex h-16 items-center justify-between">
-          <div
-            className="flex flex-1 justify-around items-center  md:items-stretch md:justify-start"
+    <nav className="relative z-10 bg-black relative md:fixed md:right-0 h-18 top-0 left-0 border-b border-gray-400 py-5">
+      <div className="container mx-auto flex items-center justify-between">
+        <div className="text-slate-300 text-xl font-bold pl-4 m-1 md:m-6 lg:m-1" style={{fontFamily:"Merienda"}}> 
+       <p className='text-sm md:text-xs lg:text-xl md:pr-4'>Navin Methani</p>
+        </div>
+       
+        
+        {/* Hamburger Icon */}
+       <section className='flex mr-5 lg:hidden'>
+       <div
+           className="HAMBURGER-ICON space-y-2"
+           onClick={() => setIsNavOpen((prev) => !prev)}
           >
+            <span className="block h-0.5 w-8  bg-slate-300"></span>
+            <span className="block h-0.5 w-8  bg-slate-300"></span>
+            <span className="block h-0.5 w-8  bg-slate-300"></span>
+          </div>
 
-            <Link className=" cursor-pointer tracking-wider italic font-mono flex text-slate-300 flex-shrink-0 items-center md:ml-10" to="home" spy={true} 
+          <div className={isNavOpen ? "showMenuNav" : "hideMenuNav"}> 
+            <div
+              className="CROSS-ICON absolute top-0 right-0 px-8 py-8"
+              onClick={() => setIsNavOpen(false)} 
+            >
+              <svg
+                className="h-8 w-8 text-gray-600"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <line x1="18" y1="6" x2="6" y2="18" />
+                <line x1="6" y1="6" x2="18" y2="18" />
+              </svg>
+            </div>
+            <ul className="NAVIGATION-MOBILE-OPEN flex flex-col items-center justify-between min-h-[250px]">
+              <li className=" my-8 uppercase">
+                <Link to ="about" 
+                 spy={true} 
+                 smooth={true} 
+                 offset={-100} 
+                 className='cursor-pointer'>About</Link>
+              </li>
+              <li className="my-8 uppercase">
+              <Link to="services"
+             
+              
+             spy={true} 
+      smooth={true} 
+      offset={-100} 
+      className='cursor-pointer'
+           >
+             Services
+           </Link>
+              </li>
+              <li className="my-8 uppercase">
+              <Link to="testimonials"
+             
+              
+             spy={true} 
              smooth={true} 
-             offset={-60}>
-             Navin
-            </Link>
+             offset={-100} 
+             className='cursor-pointer'
+           >
+             Testimonials
+           </Link>
+              </li>
+              <li className="my-8 uppercase">
+              <Link to="contact"
+             
+              
+             spy={true} 
+             smooth={true} 
+             offset={200} 
+             className='cursor-pointer'
+           >
+             Contact
+           </Link>
+              </li>
+            </ul>
+          </div>
+       </section>
+        {/* Links */}
 
-            <div className="mr-32 md:ml-auto">
-              <div className="text-slate-300 flex space-x-10">
-
-                 <Link to="hero"  spy={true} 
+        <div className="flex-col text-slate-300 space-x-10 hidden mt-4 md:flex md:flex-row md:mt-0">
+        <Link to="hero"  spy={true} 
              smooth={true} 
              offset={-60} 
              className='cursor-pointer'>Home</Link>
@@ -71,15 +136,29 @@ const Navbar = () => {
            >
              Contact
            </Link>
-
-           
-              </div>
-            </div>
-          </div>
         </div>
-     
+      </div>
+      <style>{`
+      .hideMenuNav {
+        display: none;
+      }
+      .showMenuNav {
+        display: block;
+        position: absolute;
+        width: 100%;
+        height: 100vh;
+        top: 0;
+        left: 0;
+        background: white;
+        z-index: 10;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-evenly;
+        align-items: center;
+      }
+    `}</style>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
