df7f2ceba30380d25749b3a33d18259cf222262d
Neha Methani <nehamethani54@gmail.com>
2024-11-22 23:39:54 +0530

    ABout section 3

--- src/Components/Services.jsx
+++ src/Components/Services.jsx
@@ -3,8 +3,8 @@ import Cards from './Cards';
 
 const Services = () => {
   return (
-   <div className='flex flex-col space-y-4 mb-20 ' id='services'>
-   <h1 className='text-4xl font-bold text-center text-white'>Services</h1>
+   <div className='flex flex-col space-y-4 mb-20 text-white' id='services'>
+   <h1 className="text-xl uppercase font-thin text-center" style={{fontFamily:"Raleway"}}>Services</h1>
    <h2 className='text-white text-center text-3xl font-mono font-thin tracking-wide text-lg md:text-lef'>Bringing Elite Fitness To Your Doorstep</h2>
    <br/>
    <Cards/>
