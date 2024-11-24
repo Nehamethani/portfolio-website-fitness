a045e423bd94fcdb1f8e6a561eed505e68ae4e98
Neha Methani <nehamethani54@gmail.com>
2024-11-24 23:34:04 +0530

    Refined Testimonials Section

--- src/Components/Services.jsx
+++ src/Components/Services.jsx
@@ -3,7 +3,7 @@ import Cards from './Cards';
 
 const Services = () => {
   return (
-   <div className='flex flex-col space-y-4 mb-20 p-6 pb-20 text-white' id='services'>
+   <div className='flex flex-col space-y-4  p-6 pb-20 text-white' id='services'>
    <h1 className="text-xl uppercase font-thin text-center" style={{fontFamily:"Raleway"}}>Services</h1>
    <h2 className="text-white-300 text-3xl tracking-wide text-center" style={{fontFamily:"Funnel Display"}}>Bringing Elite Fitness To Your Doorstep</h2>
    <br/>
--- src/Components/Testimonials.jsx
+++ src/Components/Testimonials.jsx
@@ -8,7 +8,7 @@ const style = {
 const Testimonials = () => {
   return (
    
-      <section className='flex neumorphic h-[60vh] bg-gray-200 p-8 items-center p-32' id='testimonials'>
+      <section className='flex flex-col lg:flex-row neumorphic bg-gray-200 p-8 items-center' id='testimonials'>
       <div className='flex basis-1/2 flex-col grow-0 space-y-6 p-4 text-left'>
       <h1 className="text-xl uppercase font-thin text-left font-bold" style={style}>Testimonials</h1>
       <h2 className="text-white-300 text-3xl tracking-wide md:text-lef" style={{fontFamily:"Funnel Display"}}>Hear What Navin's Client Says</h2>
@@ -17,23 +17,23 @@ const Testimonials = () => {
     </p>
     <button className='mt-auto self-start px-3  justify-self-end py-2 w-52 h-16 border border-black text-black hover:bg-black hover:text-white'>Discover Success Stories</button>   
     </div>
-      <div class="flex items-center justify-center bg-gray-200 space-x-3 flex-end">
+      <div class="flex flex-col lg:flex-row items-center justify-center bg-gray-200 space-x-3 space-y-4 lg:space-y-0 flex-end">
   
-  <div class="w-56 p-4 h-64  bg-gray-200 
+  <div class="w-64 p-4 h-64  bg-gray-200 
               shadow-[8px_8px_16px_rgba(0,0,0,0.15),_-8px_-8px_16px_rgba(255,255,255,0.8)]">
     <h3 class="text-lg font-semibold text-gray-800">Client 1</h3>
     <p class="mt-2 text-gray-600">"Navin has transformed my fitness routine. I feel more energetic and motivated every day!"</p>
   </div>
   
  
-  <div class="w-56 p-4 h-64 bg-gray-200 
+  <div class="w-64 p-4 h-64 bg-gray-200 
               shadow-[8px_8px_16px_rgba(0,0,0,0.15),_-8px_-8px_16px_rgba(255,255,255,0.8)]">
     <h3 class="text-lg font-semibold text-gray-800">Client 2</h3>
     <p class="mt-2 text-gray-600">"Thanks to Navin’s guidance, I’ve reached my fitness goals faster than I thought possible."</p>
   </div>
   
  
-  <div class="w-56 p-4 h-64 bg-gray-200 
+  <div class="w-64 p-4 h-64 bg-gray-200 
               shadow-[8px_8px_16px_rgba(0,0,0,0.15),_-8px_-8px_16px_rgba(255,255,255,0.8)]">
     <h3 class="text-lg font-semibold text-gray-800">Client 3</h3>
     <p class="mt-2 text-gray-600">"Navin’s workouts are challenging but effective. Highly recommended for anyone serious about fitness!"</p>
